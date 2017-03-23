f454d3d066ff44d410bb582ea988c71b983f40ed
Steven <steven@crowder.io>
2017-03-23 16:54:03 -0400

    Initial commit for BenQ projector communication web service.

---
+++ src/projector/benq/ws/projector.java
@@ -0,0 +1,20 @@
+package projector.benq.ws;
+
+import gnu.io.*;
+import java.awt.Color;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.TooManyListenersException;
+
+public class projector implements SerialPortEventListener {
+  
+
+  @Override
+  public void serialEvent(SerialPortEvent arg0) {
+	// TODO Auto-generated method stub
+	
+  }
+}
